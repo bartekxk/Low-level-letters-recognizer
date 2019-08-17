a419b3e5bd0727ba19315ecfe0198e555874899a
Bartłomiej Kocot <38502616+bartekxk@users.noreply.github.com>
2019-08-17 13:04:08 +0200

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,6 +1,6 @@
 import numpy as np
 from math import log
-def train(Theta1,Theta2,X,y,_lambda,num_labels):
+def train(Theta1,Theta2,X,y,_lambda,num_labels,alpha):
     m = np.size(X,0)
     a1 = X
     a1 = np.append(np.ones((np.size(X,0),1)),a1,axis=1)
@@ -11,18 +11,18 @@ def train(Theta1,Theta2,X,y,_lambda,num_labels):
     Theta1NoBias = np.delete(Theta1, 0, axis=1)
     Theta2NoBias = np.delete(Theta2, 0, axis=1)
     J = costFunction(Theta1NoBias,Theta2NoBias,a3,y,_lambda,num_labels,m)
-    print(J)
+    print("Cost:" + str(J))
     delta3 = a3 - y
     delta2 = np.multiply(np.dot(Theta2NoBias.getT(),delta3.getT()),sigmoidGradient(z2).getT())
-    Theta2 = np.dot(delta3.getT(),a2) / m
-    Theta1 = np.dot(delta2,a1) / m
+    Theta2 = np.subtract(Theta2,np.dot(delta3.getT(),a2) * alpha / m)
+    Theta1 = np.subtract(Theta1,np.dot(delta2,a1) * alpha / m)
     for i in range(np.size(Theta1NoBias,0)):
-        for j in range(1,np.size(Theta1NoBias,1)):
-            Theta1.itemset((i,j), Theta1.item((i,j)) + (Theta1NoBias.item((i,j)) * _lambda / m))
+        for j in range(np.size(Theta1NoBias,1)):
+            Theta1.itemset((i,j+1), Theta1.item((i,j+1)) + (Theta1NoBias.item((i,j)) * _lambda / m))
     for i in range(np.size(Theta2NoBias,0)):
-        for j in range(1,np.size(Theta2NoBias,1)):
-            Theta2.itemset((i,j), Theta2.item((i,j)) + (Theta2NoBias.item((i,j)) * _lambda / m))
-    return Theta1,Theta2
+        for j in range(np.size(Theta2NoBias,1)):
+            Theta2.itemset((i,j+1), Theta2.item((i,j+1)) + (Theta2NoBias.item((i,j)) * _lambda / m))
+    return Theta1,Theta2,J
 def costFunction(Theta1NoBias,Theta2NoBias,a,y,_lambda,num_labels,m):
     J = 0
     for i in range(m):
@@ -49,5 +49,8 @@ theta2 = np.asmatrix(np.random.rand(26,31))
 for i in range(np.size(y,0)):
     y_tmp[i,int(y[i,0])-1] = 1
 y = y_tmp
-for i in range(100):
-    theta1, theta2 = train(theta1,theta2,X,y,1,num_labels)
+J = 100000000
+while J>0.0001:
+    theta1, theta2, J = train(theta1,theta2,X,y,0.1,num_labels,0.1)
+np.savetxt('theta1.data', theta1)
+np.savetxt('theta2.data', theta2)
