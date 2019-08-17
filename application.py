5fe0df1601442863d030b0127d97875a81db9bf4
Bartłomiej Kocot <38502616+bartekxk@users.noreply.github.com>
2019-08-17 02:56:31 +0200

    Create application.py

---
+++ application.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+# Import necessary modules
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+from sklearn import model_selection
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
